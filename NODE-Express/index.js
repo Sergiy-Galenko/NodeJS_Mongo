e698a27d85b09ae1ebc6937b684bb293628579a7
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-21 00:58:17 +0300

    update + mongoDB

--- NODE-Express/index.js
+++ NODE-Express/index.js
@@ -1,32 +1,42 @@
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const homeRoutes = require('./routes/home')
-const cardRoutes = require('./routes/card')
-const addRoutes = require('./routes/add')
-const coursesRoutes = require('./routes/courses')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const cardRoutes = require("./routes/card");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
-})
+    defaultLayout: "main",
+    extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', homeRoutes)
-app.use('/add', addRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/card', cardRoutes)
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+    try {
+        const url = `mongodb+srv://sgalenko783:PvUmPdbzdR37kgL@cluster0.vr3i46m.mongodb.net/shop`;
+        await mongoose.connect(url, { useNewUrlParser: true });
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+start();
--- NODE-Express/routes/add.js
+++ NODE-Express/routes/add.js
@@ -1,20 +1,26 @@
-const {Router} = require('express')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
-router.get('/', (req, res) => {
-  res.render('add', {
-    title: 'Добавить курс',
-    isAdd: true
-  })
-})
+router.get("/", (req, res) => {
+    res.render("add", {
+        title: "Добавить курс",
+        isAdd: true,
+    });
+});
 
-router.post('/', async (req, res) => {
-  const course = new Course(req.body.title, req.body.price, req.body.img)
+router.post("/", async (req, res) => {
+    const course = new Course({
+        title: req.body.title,
+        price: req.body.price,
+        img: req.body.img,
+    });
+    try {
+        await course.save();
+        res.redirect("/courses");
+    } catch (e) {
+        console.log(e);
+    }
+});
 
-  await course.save()
-
-  res.redirect('/courses')
-})
-
-module.exports = router
+module.exports = router;
