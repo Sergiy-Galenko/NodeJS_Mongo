33e068140ba5d80c2faf6a2f2fb0c31e3ea71acb
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-26 01:21:02 +0300

    Setting up a mail service

---
+++ NODE-Express/keys/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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'
+}
