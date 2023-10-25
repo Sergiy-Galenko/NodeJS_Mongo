0127b85f15c44a74629c891f5ebc6ef00274f19b
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-26 00:24:18 +0300

    Database session

--- NODE-Express/index.js
+++ NODE-Express/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
@@ -12,11 +13,15 @@ const authRoutes = require('./routes/auth')
 const User = require('./models/user')
 const varMiddleware = require('./middleware/variables')
 
+const MONGODB_URI = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
 const app = express()
-
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
+})
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -28,7 +33,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: 'some secret value',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }))
 app.use(varMiddleware)
 
@@ -43,20 +49,10 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
-    await mongoose.connect(url, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false
     })
-    // const candidate = await User.findOne()
-    // if (!candidate) {
-    //   const user = new User({
-    //     email: 'vladilen@mail.ru',
-    //     name: 'Vladilen',
-    //     cart: {items: []}
-    //   })
-    //   await user.save()
-    // }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
