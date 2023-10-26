ef55f22559bbbc5aec27d90b93a6c1880707112e
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-26 23:19:52 +0300

    Sending letter

--- NODE-Express/keys/index.js
+++ NODE-Express/keys/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop',
   SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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'
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
 }
