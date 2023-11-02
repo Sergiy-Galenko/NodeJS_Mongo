c75cea751a3743bcc90d4a5729cd49e6573c4d7f
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-11-02 17:02:53 +0200

    Receiving data

--- GraphQL/graphql/resolver.js
+++ GraphQL/graphql/resolver.js
@@ -1,5 +1,38 @@
+const Todo = require('../models/todo')
+
+const users = [
+  {name: 'Igor', age: 30, email: 'igor@mail.ru'},
+  {name: 'Elena', age: 23, email: 'elena@gmail.com'}
+]
+
 module.exports = {
   test() {
-    return 'Hello GraphQL!'
+    return {
+      count: Math.trunc(Math.random() * 10),
+      users
+    }
+  },
+  random({min, max, count}) {
+    const arr = []
+    for (let i = 0; i < count; i++) {
+      const random = Math.random() * (max - min) + min
+      arr.push(random)
+    }
+    return arr
+  },
+  addTestUser({user: {name, email}}) {
+    const user = {
+      name, email,
+      age: Math.ceil(Math.random() * 30)
+    }
+    users.push(user)
+    return user
+  },
+  async getTodos() {
+    try {
+      return await Todo.findAll()
+    } catch (e) {
+      throw new Error('Fetch todos is not available')
+    }
   }
 }
